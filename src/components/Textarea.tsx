d367fa1018d238d37681048dae61a5b57d76bdf1
BlishchAndrew <blishch.andrew@gmail.com>
2022-07-29 16:23:43 +0300

    first commit

---
+++ src/components/Textarea.tsx
@@ -0,0 +1,29 @@
+import {FC, FocusEvent, useState} from 'react'
+
+// type InputProps = {
+//    name: string;
+// }
+
+const Input: FC = (props) => {
+   const [isFocus, setIsFocus] = useState(false)
+   const { label, errorMessage, onChange, ...inputProps } = props;
+
+   const handlerError = (e: FocusEvent<HTMLInputElement>) => {
+      setIsFocus(true)
+   }
+
+   return (
+      <div className="feedback-form__line">
+         <label className="feedback-form__label" htmlFor="name">{label}<span>*</span></label>
+         <input
+            {...inputProps}
+            className="feedback-form__input"
+            onBlur={handlerError}
+            onChange={onChange}
+         />
+         {isFocus &&  <div className="feedback-form__text-error">{errorMessage}</div>}
+      </div>
+   );
+}
+
+export default Input;
